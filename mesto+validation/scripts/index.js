52ac7bb09707bb7c6758e71650f16eb7ea458efc
alndis <148362356+alndis@users.noreply.github.com>
2024-12-03 18:05:24 +0300

    5 pr

--- mesto+validation/scripts/index.js
+++ mesto+validation/scripts/index.js
@@ -100,8 +100,6 @@ function createCardDefault(){
     });
   }
 
-  ф
-
   return cardsArray;
 }
 function addCardToPage(card){
@@ -134,8 +132,130 @@ const addButton = document.querySelector('.profile__add-button');
 for (let i = 0; i< cardsArray.length; i++){
   placesList.append(cardsArray[i]);
 }
-///////////////////////////////////////////раскрытие картинки
+/////////////////////////////Валидация формы добавления карточки
+const newCardPopupTitleInput = document.querySelector(".popup__input_type_card-name");
+const newCardPopupUrlInput = document.querySelector(".popup__input_type_url");
+const cardAddButton = document.querySelector(".popup_type_new-card").querySelector(".popup__button");
+const cardNameInputInvalid = document.querySelector('.title__input_invalid');
+const cardUrlInputInvalid = document.querySelector('.url__input_invalid');
+newCardPopupTitleInput.addEventListener('input', function(evt){ //Валидация названия
+  if (!evt.target.validity.valid){
+    evt.target.classList.add("popup__input-invalid"); //изменение цвета границы
+    cardAddButton.classList.add("popup__close-invalid"); //изменение прозрачности кнопки
+    cardAddButton.disabled = true;
+  }
+  else{
+    evt.target.classList.remove("popup__input-invalid");
+    if (newCardPopupUrlInput.validity.valid){
+      cardAddButton.classList.remove("popup__close-invalid");
+      cardAddButton.disabled = false;
+    }
+  }
 
+  if (evt.target.validity.tooShort){ //Слишком коротко
+    cardNameInputInvalid.textContent = "Минимальное количество символов: 2. Длина текста сейчас: " + evt.target.value.length; 
+  }
+  else if(evt.target.validity.valid){
+    cardNameInputInvalid.textContent = "";
+  }
 
+  if (evt.target.value.length === 0){ //Пустое
+    cardNameInputInvalid.textContent = "Вы пропустили это поле";
+  }
+});
 
+newCardPopupUrlInput.addEventListener("input", function(evt){ //Валидация ссылки
+  if (!evt.target.validity.valid){
+    evt.target.classList.add("popup__input-invalid");
+    cardAddButton.classList.add("popup__close-invalid");
+    cardAddButton.disabled = true;
+  }
+  else{
+    evt.target.classList.remove("popup__input-invalid");
+    if (newCardPopupTitleInput.validity.valid){
+      cardAddButton.classList.remove("popup__close-invalid");
+      cardAddButton.disabled = false;
+    }
+  }
 
+  if (!evt.target.validity.valid){
+    cardUrlInputInvalid.textContent = "Введите ссылку";
+  }
+  else if(evt.target.validity.valid){
+    cardUrlInputInvalid.textContent = "";
+  }
+
+  if (evt.target.value.length === 0){ //Пустое
+    cardUrlInputInvalid.textContent = "Вы пропустили это поле";
+  }
+});
+/////////////////////////////Валидация формы изменения профиля
+const profilePopupTitleInput = document.querySelector(".popup__input_type_name");
+const profilePopupDescriptionInput = document.querySelector(".popup__input_type_description");
+const profileSaveButton = document.querySelector(".popup_type_edit").querySelector(".popup__button");
+const profileTitleInputInvalid = document.querySelector('.name__input_invalid');
+const profileDescriptionInputInvalid = document.querySelector('.description__input_invalid');
+
+profilePopupTitleInput.addEventListener("input", function(evt){
+  if (!evt.target.validity.valid){
+    evt.target.classList.add("popup__input-invalid"); //изменение цвета границы
+    profileSaveButton.classList.add("popup__close-invalid"); //изменение прозрачности кнопки
+    profileSaveButton.disabled = true;
+  }
+  else{
+    evt.target.classList.remove("popup__input-invalid");
+    if (profilePopupDescriptionInput.validity.valid){
+      profileSaveButton.classList.remove("popup__close-invalid");
+      profileSaveButton.disabled = false;
+    }
+  }
+  if (evt.target.validity.tooShort){ //Слишком коротко
+    profileTitleInputInvalid.textContent = "Минимальное количество символов: 2. Длина текста сейчас: " + evt.target.value.length; 
+  }
+  else if(evt.target.validity.valid){
+    profileTitleInputInvalid.textContent = "";
+  }
+
+  if (evt.target.value.length === 0){ //Пустое
+    profileTitleInputInvalid.textContent = "Вы пропустили это поле";
+  }
+});
+
+profilePopupDescriptionInput.addEventListener("input", function(evt){
+  if (!evt.target.validity.valid){
+    evt.target.classList.add("popup__input-invalid"); //изменение цвета границы
+    profileSaveButton.classList.add("popup__close-invalid"); //изменение прозрачности кнопки
+    profileSaveButton.disabled = true;
+  }
+  else{
+    evt.target.classList.remove("popup__input-invalid");
+    if (profilePopupTitleInput.validity.valid){
+      profileSaveButton.classList.remove("popup__close-invalid");
+      profileSaveButton.disabled = false;
+    }
+  }
+  if (evt.target.validity.tooShort){ //Слишком коротко
+    profileDescriptionInputInvalid.textContent = "Минимальное количество символов: 2. Длина текста сейчас: " + evt.target.value.length; 
+  }
+  else if(evt.target.validity.valid){
+    profileDescriptionInputInvalid.textContent = "";
+  }
+
+  if (evt.target.value.length === 0){ //Пустое
+    profileDescriptionInputInvalid.textContent = "Вы пропустили это поле";
+  }
+
+});
+////////////////////////////////////////Закрытие на ESC
+const popupsList = document.querySelectorAll(".popup");
+
+for (let i = 0; i < popupsList.length; i++){
+  popupsList[i].addEventListener("keydown", function(evt){
+    if (evt.key === "Escape"){
+      popupsList[i].classList.remove("popup_is-opened");
+    }
+  });
+  popupsList[i].addEventListener("click", function(evt){
+    evt.target.classList.remove("popup_is-opened");
+  });
+}
